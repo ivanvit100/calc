a2f97e10499f0870e9ed7a3501815cdd40f6baf6
ivanvit100 <ivanvit100@gmail.com>
2022-07-27 19:57:07 +0300

    PWA update

--- service.js
+++ service.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v1';
-var CACHE_DYNAMIC_NAME = 'dynamic-v1';
+var CACHE_STATIC_NAME = 'static-v2';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
 
 self.addEventListener('install', function (event) {
   console.log('Installing Service Worker ...');

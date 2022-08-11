9f600a25038797a57ecebb57b8bd93cf26b3a9c4
ivanvit100 <ivanvit100@gmail.com>
2022-08-11 17:24:01 +0300

    Service update test

--- service.js
+++ service.js
@@ -1,70 +1,49 @@
-var CACHE_STATIC_NAME = 'static-v2.8';
-var CACHE_DYNAMIC_NAME = 'dynamic-v2.8';
+var cacheName = "v2.8"
 
-self.addEventListener('install', function (event) {
-  console.log('Installing Service Worker ...');
-  event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-      .then(function (cache) {
-        console.log('Precaching App Shell');
-        cache.addAll([
-          './index.html',
-          './css/style.css',
-          './css/link.png',
-          './css/select.png',
-          './img/dl.png',
-          './img/logo.png',
-          './img/light.png',
-          './img/dark.png',
-          './img/arrow.png',
-          './img/preview.png',
-          './js/script.js',
-          './js/detect.js',
-          './js/vue.js',
-          './js/vue-script.js'
-        ]);
-      })
-  )
+const appShellFiles = [
+  './index.html',
+  './css/style.css',
+  './css/link.png',
+  './css/select.png',
+  './img/dl.png',
+  './img/logo.png',
+  './img/light.png',
+  './img/dark.png',
+  './img/arrow.png',
+  './img/preview.png',
+  './js/script.js',
+  './js/detect.js',
+  './js/vue.js',
+  './js/vue-script.js'
+];
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker]: Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker]: Caching content');
+    await cache.addAll(contentToCache);
+  })());
 });
 
-self.addEventListener('activate', function (event) {
-  console.log('Activating Service Worker ....');
-  event.waitUntil(
-    caches.keys()
-      .then(function (keyList) {
-        return Promise.all(keyList.map(function (key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-  return self.clients.claim();
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker]: Fetching resource: ${e.request.url}`);
+    if (r) { return r; }
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker]: Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
 });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the network
-    fetch(event.request)
-      .then(function(res) {
-        return caches.open(CACHE_DYNAMIC_NAME)
-          .then(function(cache) {
-            // Put in cache if succeeds
-            cache.put(event.request.url, res.clone());
-            return res;
-          })
-      })
-      .catch(function(err) {
-          // Fallback to cache
-          event.respondWith(fromCache(event.request));
-      })
-  );
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => {
+      if (key === cacheName) { return; }
+      return caches.delete(key);
+    }))
+  }));
 });
-
-function fromCache(request) {
-    return caches.open(CACHE_DYNAMIC_NAME).then((cache) =>
-        cache.match(request).then((matching) =>
-            matching || Promise.reject('no-match')
-        ));
-}

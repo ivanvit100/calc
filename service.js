38da05f6e586b7038f42d8c8c808fad6f12bbb2f
ivanvit100 <ivanvit100@gmail.com>
2022-08-12 14:49:44 +0300

    Service test update

--- service.js
+++ service.js
@@ -1,6 +1,6 @@
-var cacheName = "v2.8"
+let cache_name = "v2.9"; 
 
-const appShellFiles = [
+const assets = [
   './index.html',
   './css/style.css',
   './css/link.png',
@@ -17,33 +17,30 @@ const appShellFiles = [
   './js/vue-script.js'
 ];
 
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker]: Install');
-  e.waitUntil((async () => {
-    const cache = await caches.open(cacheName);
-    console.log('[Service Worker]: Caching content');
-    await cache.addAll(contentToCache);
-  })());
+self.addEventListener("install", event => {
+    console.log("installing...");
+    event.waitUntil(
+        caches
+            .open(cache_name)
+            .then(cache => {
+                return cache.addAll(assets);
+            })
+            .catch(err => console.log(err))
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith((async () => {
-    const r = await caches.match(e.request);
-    console.log(`[Service Worker]: Fetching resource: ${e.request.url}`);
-    if (r) { return r; }
-    const response = await fetch(e.request);
-    const cache = await caches.open(cacheName);
-    console.log(`[Service Worker]: Caching new resource: ${e.request.url}`);
-    cache.put(e.request, response.clone());
-    return response;
-  })());
-});
-
-self.addEventListener('activate', (e) => {
-  e.waitUntil(caches.keys().then((keyList) => {
-    return Promise.all(keyList.map((key) => {
-      if (key === cacheName) { return; }
-      return caches.delete(key);
-    }))
-  }));
+self.addEventListener("fetch", event => {
+    if (event.request.url === "https://calc.ivanvit.ru/") {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                self.cache.open(cache_name).then(cache => cache.match("./index.html"))
+            )
+        );
+    } else {
+        event.respondWith(
+            fetch(event.request).catch(err =>
+                caches.match(event.request).then(response => response)
+            )
+        );
+    }
 });

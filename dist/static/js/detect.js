7d3797f0f61ff19ffd022e10b36cd37926b3e426
ivanvit100 <ivanvit100@gmail.com>
2022-10-06 14:16:48 +0300

    Dist update

---
+++ dist/static/js/detect.js
@@ -0,0 +1,47 @@
+/*Скрипт для обнаружения нажатий проекта "Калькулятор | ivanvit.ru"
+Project: https://calc.ivanvit.ru
+
+GitHub: https://github.com/ivanvit100
+E-Mail: develope@ivanvit.ru */
+
+document.addEventListener('keydown', function(event){
+	//Функция обнаружения нажатий клавиш физической клавиатуры
+	if(event.key >= 0 && event.key <= 9){
+		/*Числовые клавишы вызывают функцию numAdd()
+		посредством имитации нажатия на соответствующую
+		кнопку класса num*/
+		el = document.querySelector(".num" + event.key);
+	}
+	else{
+		/*Прочие клавиши перехватываются и направляются 
+		по спецефичному ID*/
+		switch(event.key){
+			case '/':
+			el = document.querySelector("#dl");
+			break;
+			case '*':
+			el = document.querySelector("#mn");
+			break;
+			case '(':
+			el = document.querySelector("#sc");
+			break;
+			case ')':
+			el = document.querySelector("#sc2");
+			break;
+			default:
+			/*Если клавиши кнопки с подобным event.key не существует,
+			производится попытка вызвать нажатие на кнопку
+			с соответствующим event.code (Enter, Backspace, Comma etc.)*/
+			el = document.querySelector("." + event.code);
+			break;
+		}
+	}
+	try{
+		/*Конструкция try catch применяется с целью избежать ошибок
+		в консоли при нажатии на клавишу, не имеющую соответствующего ей
+		элемента в интерфейсе, а также для идентификации такой клавиши.*/
+		el.click();
+		el.classList.add("hover");
+		intervalID = setTimeout(function(){el.classList.remove("hover")}, 200);
+	}catch(e){console.warn("[Keyboard]: Нет команды для клавиши: " + event.key + "/" + event.code)}
+});
